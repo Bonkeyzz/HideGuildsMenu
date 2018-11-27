ae514951b0b0275b36bcb88b2952310d0d157c4a
Bonkeyzz <bonkeyzz10@gmail.com>
2018-11-27 21:37:33 +0200

    Update HideGuildMenu.plugin.js
    Renamed guilds_fadeout, guilds_fadein to guilds_slideout, guilds_slidein

--- HideGuildMenu.plugin.js
+++ HideGuildMenu.plugin.js
@@ -22,47 +22,47 @@ class HideGuilds {
 
     start() {
 		//TODO: Improve this part.
-		var sHtmlDark = "@keyframes guilds_fadeout {";
+		var sHtmlDark = "@keyframes guilds_slideout {";
 		sHtmlDark += "from { width: " + this.org_width + "; }";
 		sHtmlDark += "to { width: 0.5% }";
 		sHtmlDark += "}";
 		
-		sHtmlDark += "@keyframes guilds_fadein {";
+		sHtmlDark += "@keyframes guilds_slidein {";
 		sHtmlDark += "from { width: 0.5%; }";
 		sHtmlDark += "to { width: " + this.org_width + " }";
 		sHtmlDark += "}";
 		
 		sHtmlDark += ".theme-dark .guildsWrapper-5TJh6A{";
 		sHtmlDark += "width: " + this.org_width + ";";
-		sHtmlDark += "animation-name: guilds_fadein;";
+		sHtmlDark += "animation-name: guilds_slidein;";
 		sHtmlDark += "animation-duration: 0.5s;";
 		sHtmlDark += "}";
 		
 		sHtmlDark += ".theme-dark .guildsWrapper-5TJh6A:not(:hover){";
 		sHtmlDark += "width: 0.5%;";
-		sHtmlDark += "animation-name: guilds_fadeout;";
+		sHtmlDark += "animation-name: guilds_slideout;";
 		sHtmlDark += "animation-duration: 0.5s;";
 		sHtmlDark += "}";
 		
-		var sHtmlLight = "@keyframes guilds_fadeout {";
+		var sHtmlLight = "@keyframes guilds_slideout {";
 		sHtmlLight += "from { width: " + this.org_width + "; }";
 		sHtmlLight += "to { width: 0.5% }";
 		sHtmlLight += "}";
 		
-		sHtmlLight += "@keyframes guilds_fadein {";
+		sHtmlLight += "@keyframes guilds_slidein {";
 		sHtmlLight += "from { width: 0.5%; }";
 		sHtmlLight += "to { width: " + this.org_width + " }";
 		sHtmlLight += "}";
 		
 		sHtmlLight += ".theme-light .guildsWrapper-5TJh6A{";
 		sHtmlLight += "width: " + this.org_width + ";";
-		sHtmlLight += "animation-name: guilds_fadein;";
+		sHtmlLight += "animation-name: guilds_slidein;";
 		sHtmlLight += "animation-duration: 0.5s;";
 		sHtmlLight += "}";
 		
 		sHtmlLight += ".theme-light .guildsWrapper-5TJh6A:not(:hover){";
 		sHtmlLight += "width: 0.5%;";
-		sHtmlLight += "animation-name: guilds_fadeout;";
+		sHtmlLight += "animation-name: guilds_slideout;";
 		sHtmlLight += "animation-duration: 0.5s;";
 		sHtmlLight += "}";
 		
